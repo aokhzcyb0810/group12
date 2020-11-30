fcc5ff3f7424e1b4a42a36267351aadaafa9d0e0
cyb <7554454+cyb1234dddd@user.noreply.gitee.com>
2020-11-30 10:25:43 +0800

    git push -u origin master #	new file:   src/views/Search.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+<el-container>
+  <el-header >Header</el-header>
+  <el-main>Main</el-main>
+</el-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"home"
+    }
+</script>
+
+<style lang="less" scoped>
+.el-header{
+    height: 60px;
+    background-color:aliceblue;
+    border-block-color: black;
+}
+</style>
+
