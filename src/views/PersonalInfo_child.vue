142c70df225d48a48583ebbb92843286b0bf08a5
YuigahamaYui0618 <63180028+YuigahamaYui0618@users.noreply.github.com>
2020-12-17 19:14:55 +0800

    Update PersonalInfo_child.vue

--- src/views/PersonalInfo_child.vue
+++ src/views/PersonalInfo_child.vue
@@ -2,7 +2,7 @@
     <div>
         <div>
             <el-row>
-                <el-col span=8>
+                <el-col :span="8">
                     <div><span style="margin-left: 5%"><i class="el-icon-info"> 个人信息</i></span></div>
                 </el-col>
                 <el-col :span="12">
@@ -18,7 +18,7 @@
                 </el-col>
                 <el-col :span="4">
                     <div>
-                        <el-button type="primary" v-show="isScholar" size="mini" style="float: right">发送私信</el-button>
+                        <el-button type="primary" v-show="isScholar" size="mini" style="float: right" @click="sendMessage">发送私信</el-button>
                     </div>
                 </el-col>
 
@@ -34,9 +34,6 @@
                                 </el-form-item>
                                 <el-form-item label="姓名">
                                     <el-input v-model="form.name" style="width: 300px" size="small"></el-input>
-                                </el-form-item>
-                                <el-form-item label="学者id">
-                                    <el-input v-model="form.sid" disabled style="width: 300px" size="small"></el-input>
                                 </el-form-item>
                                 <el-form-item
                                         prop="email"
@@ -54,10 +51,16 @@
                             </el-form>
                             <span style="margin-left: 5%">编辑头像</span>
                             <div>
-                                <el-avatar :size="100" style="margin-left: 40%;margin-top: 5%"></el-avatar>
-                            </div>
-                            <div>
-                                <el-button style="margin-left: 43%;margin-top: 5%">保存</el-button>
+                                <el-upload
+                                        class="avatar-uploader"
+                                        action="https://jsonplaceholder.typicode.com/posts/"
+                                        :show-file-list="false"
+                                        :on-success="handleAvatarSuccess"
+                                        :before-upload="beforeAvatarUpload"
+                                        style="margin-left: 30%">
+                                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                                </el-upload>
                             </div>
                             <div style="text-align: center;margin-top: 3%">
                                 <h4 v-show="!isScholar">你还未进行学者认证，<el-link type="primary" style="font-size: 16px" @click="toIdentify">点击这里</el-link>进行认证</h4>
@@ -68,26 +71,26 @@
                     <td style="width: 48%">
                         <div style="margin-top: 0%;height: 700px">
                             <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" style="margin-top: 0px">
-                                <el-menu-item index="1">系统通知</el-menu-item>
-                                <el-menu-item index="2">学者私信</el-menu-item>
-                                <el-menu-item index="3">我的私信</el-menu-item>
+                                <el-menu-item index="1" @click="showSysMessagelist">系统通知</el-menu-item>
+                                <el-menu-item index="2" @click="showScoMessagelist">学者私信</el-menu-item>
+                                <el-menu-item index="3" @click="showMineMessagelist">我的私信</el-menu-item>
                             </el-menu>
                             <div style="height: 5px"></div>
-                            <div v-for="(item,index) in message" :key="item" @click="viewMessage(index)">
+                            <div v-for="(item,index) in message" :key="item" @click="viewMessage(item)">
                                 <table style="width: 100%">
                                     <tr style="height: 60px">
                                         <td style="width: 10%">
                                             <div style="height: 60px;font-size: 35px">
-                                                <i  v-show="!item.isread" class="el-icon-s-release" style="color: red;margin-bottom: 5px;margin-left: 5px;margin-top: 10px"></i>
-                                                <i  v-show="item.isread" class="el-icon-s-claim" style="color: lawngreen;margin-bottom: 5px;margin-left: 5px;margin-top: 10px"></i>
+                                                <i  v-show="item.isread = 0" class="el-icon-s-release" style="color: red;margin-bottom: 5px;margin-left: 5px;margin-top: 10px"></i>
+                                                <i  v-show="item.isread = 1" class="el-icon-s-claim" style="color: lawngreen;margin-bottom: 5px;margin-left: 5px;margin-top: 10px"></i>
                                             </div>
                                         </td>
                                         <td>
                                             <div style="margin-left: 1%;margin-top:1px;font-size: 14px">
-                                                {{item.sender}}
+                                                {{item.from}}
                                             </div>
                                             <div style="margin-left: 1%;margin-top: 10px;font-size: 12px">
-                                                {{item.content}}
+                                                {{item.text}}
                                             </div>
                                         </td>
                                     </tr>
@@ -98,21 +101,38 @@
                     </td>
                 </tr>
             </table>
-            <el-dialog title="回复私信" :visible.sync="dialogFormVisible">
+
+
+            <el-dialog title="发送私信" :visible.sync="dialogFormVisible2"><!--发送私信-->
+                <el-form :model="send_message">
+                    <el-form-item label="收件人">
+                        <el-input style="width: 45%; margin-left: 2%" v-model="send_message.to" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="私信内容">
+                        <el-input type="textarea" style="width: 85%" v-model="send_message.text" autocomplete="off"></el-input>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                    <el-button @click="dialogFormVisible2 = false">取 消</el-button>
+                    <el-button type="primary" @click="submitSend">确 定</el-button>
+                </div>
+            </el-dialog>
+
+            <el-dialog title="回复私信" :visible.sync="dialogFormVisible"><!--回复私信-->
                 <el-form :model="message[select]">
-                    <el-form-item label="寄件人" :label-width="formLabelWidth">
-                        {{message[select].sender}}
+                    <el-form-item label="寄件人">
+                        {{message[select].from}}
                     </el-form-item>
-                    <el-form-item label="私信内容" :label-width="formLabelWidth">
-                        {{message[select].content}}
+                    <el-form-item label="私信内容">
+                        {{message[select].text}}
                     </el-form-item>
-                    <el-form-item label="回复内容" :label-width="formLabelWidth">
-                        <el-input v-model="message[select].reply"></el-input>
+                    <el-form-item label="回复内容">
+                        <el-input type="textarea" v-model="this.reply"></el-input>
                     </el-form-item>
                 </el-form>
                 <div slot="footer" class="dialog-footer">
                     <el-button @click="dialogFormVisible = false">取 消</el-button>
-                    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+                    <el-button type="primary" @click="submitReply">确 定</el-button>
                 </div>
             </el-dialog>
 
@@ -139,42 +159,86 @@
         },
         data() {
             return {
+                imageUrl: '',//头像
                 isuser:false,
                 isScholar:true,//是否是认证学者，是则为true
                 activeIndex: '1',
-                dialogFormVisible: false,//发送私信的dialog
+                dialogFormVisible: false,//回复私信的dialog
+                dialogFormVisible2: false,//发送私信的dialog
                 formLabelWidth: '120px',
                 select:0,//选择查看哪一个消息
+                reply:'',//回复内容
                 form: {
-                    id: '123456789',
-                    sid: '',
-                    name: this.$store.state.userid,
+                    id: this.$store.state.userid,
+                    name: '',
                     email: '',
                     info: ''
                 },
                 //message数组看实际情况加载内容，比如在“系统通知”则加载系统通知，切换其它目录时清空后重新加载
-                message:[{
-                    sender:'系统',
-                    content:'通知1',
-                    isread:true,//消息是否已读，已读为true
-                    reply:''
-                },{
-                    sender:'系统',
-                    content:'通知2',
-                    isread:false,
-                    reply:''
-                },{
-                    sender:'系统',
-                    content:'通知3',
-                    isread:false,
-                    reply:''
-                }]
+                message:[],
+                send_message:{
+                    to: '',
+                    text: ''
+                }
             }
         },
         created(){
-            this.showuerinfo()
+            this.showUserinfo();
+            this.showSysMessagelist();
         },
         methods: {
+            showUserinfo () {
+                const res= this.$axios({
+                    method:'get',
+                    url:'/user/getUser'
+                }).catch(err=>{console.log(err)})
+
+                this.form.name=res.data.name
+                this.form.email=res.data.email
+                this.form.info=res.data.info
+                return 0
+            },
+            showSysMessagelist() {
+                const res= this.$axios({
+                    method:'get',
+                    url:'/message/sys'
+                }).catch(err=>{console.log(err)})
+                this.message = res.data
+
+                return 0
+            },
+            showScoMessagelist() {
+                const res= this.$axios({
+                    method:'get',
+                    url:'/message/fromre'
+                }).catch(err=>{console.log(err)})
+                this.message = res.data
+                return 0
+            },
+            showMineMessagelist() {
+                const res= this.$axios({
+                    method:'get',
+                    url:'/message/mine'
+                }).catch(err=>{console.log(err)})
+                this.message = res.data
+                return 0
+            },
+            handleAvatarSuccess(res, file) {
+                this.imageUrl = res.avatar
+                //this.imageUrl = URL.createObjectURL(file.raw);
+            },
+            beforeAvatarUpload(file) {
+                const isJPG = file.type === 'image/jpeg';
+                const isLt2M = file.size / 1024 / 1024 < 2;
+
+                if (!isJPG) {
+                    this.$message.error('上传头像图片只能是 JPG 格式!');
+                }
+                if (!isLt2M) {
+                    this.$message.error('上传头像图片大小不能超过 2MB!');
+                }
+                return isJPG && isLt2M;
+            },
             toIdentify(){
                 this.$router.replace(
                     {path:"",
@@ -183,85 +247,68 @@
                         }
                     })
             },
-            viewMessage(index){
-                this.dialogFormVisible=true;
-                this.select=index;
-                if(this.message[index].isread==false)
-                    this.message[index].isread=true;
-
+            sendMessage() {
+                this.dialogFormVisible2 = true
             },
-            /*async Submit() {
-                var usex
-                if(this.form.sex=='男')
-                    usex=1
-                else if(this.form.sex=='女')
-                    usex=0
-                console.log(this.form.sex)
-                const res= await this.$axios({
-                    type:'formData',
+            viewMessage(item){
+                let postData = {
+                    'Mid': item.id
+                }
+                this.$axios.post('/message/read', postData).then((response) => {
+                    for(let i = 0; i < this.message.length; i++){
+                        if(message[i].id == response.data.id){
+                            this.select = i;
+                            break;
+                        }
+                    }
+                    this.message[select].isread = 1;//改变已读状态
+                    this.dialogFormVisible = true;
+                })
+
+                this.$axios({//将新的状态返回
                     method:'post',
-                    url:'/user/info/change',
+                    url:'/message/read',
                     data:{
-                        Name:this.$store.state.userid,
-                        Email:this.form.email,
-                        Area:this.form.region,
-                        Birthday:this.form.birth,
-                        Sex:usex,
-                        Sign:this.form.info
+                        Mid: item.id
                     }
                 }).catch(err=>{console.log(err)})
-                console.log(res)
-                alert('修改完成!');//这里还要保存修改后的数据
-                this.showuerinfo()
+
+
             },
-            async showuerinfo(){
-                //  const res
-                var nowname=''
-                if(this.$route.query.Name)
-                    nowname=this.$route.query.Name
-                else
-                    nowname=this.form.name
-                //  console.log(this.$route.Name)
-                if(nowname==this.$store.state.userid){
-                    const res= await this.$axios({
-                        method:'get',
-                        url:'/user/info'
-                    }).catch(err=>{console.log(err)})
-                    console.log(res)
-                    this.form.name=res.data.Name
-                    this.form.birth=res.data.Birthday
-                    this.form.email=res.data.Email
-                    this.form.region=res.data.Area
-                    this.form.info=res.data.Sign
-                    var usex
-                    if(res.data.Sex==1)
-                        this.form.sex="男"
-                    else if(res.data.Sex==0)
-                        this.form.sex="女"
-                    console.log('fasddasd')
-                    this.isuser=false
+            submitReply() {
+                this.$axios({//将回复内容返回
+                    method:'post',
+                    url:'/message/response',
+                    data:{
+                        from: item.from,
+                        text: this.reply
+                    }
+                }).catch(err=>{console.log(err)})
+                this.dialogFormVisible = false;
+            },
+            submitSend() {
+                this.$axios({//将发送内容返回
+                    method:'post',
+                    url:'/message/send',
+                    data:{
+                        to: this.send_message.to,
+                        text: this.send_message.text
+                    }
+                }).catch(err=>{console.log(err)})
+                this.dialogFormVisible2 = false;
+            },
+            Submit(){
+                let postData = {
+                    'id': parseInt(this.$store.state.userID),
+                    'name': this.form.name,
+                    'email': this.form.email,
+                    'info': this.form.info
                 }
-                else{
-                    var uid=parseInt(this.$route.query.Uid)
-                    const res= await this.$axios({
-                        method:'get',
-                        url:'/user/otherinfo',
-                        params:{
-                            Uid:uid
-                        }
-                    }).catch(err=>{console.log(err)})
-                    console.log(res)
-                    this.form.name=res.data.Name
-                    this.form.email=res.data.Email
-                    this.form.info=res.data.Sign
-                    var usex
-                    if(res.data.Sex==1)
-                        this.form.sex="男"
-                    else if(res.data.Sex==0)
-                        this.form.sex="女"
-                    this.isuser=true
-                }
-            }*/
+                this.$axios.post('/api/user/edit', postData).catch(err=>{console.log(err)})
+                this.showUserinfo()
+            }
+
+
 
         }
     }
@@ -279,6 +326,29 @@
     .el-divider--horizontal{
         margin: 12px 3px;
         border-top: 1px dashed #e8eaec;
+    }
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+    .avatar-uploader .el-upload:hover {
+        border-color: #409EFF;
+    }
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+    }
+    .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
     }
 
 </style>
