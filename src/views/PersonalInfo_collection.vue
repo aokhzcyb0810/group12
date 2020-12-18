cef2b9ff96ee301df5ac2d6f610d6d7e0fd7acea
YuigahamaYui0618 <63180028+YuigahamaYui0618@users.noreply.github.com>
2020-12-18 11:57:36 +0800

    Update PersonalInfo_collection.vue

--- src/views/PersonalInfo_collection.vue
+++ src/views/PersonalInfo_collection.vue
@@ -19,8 +19,8 @@
 
             <el-divider></el-divider>
             <div>
-                <div v-for="(item,index) in collection_list" :key="item" @click="viewCollection(index)" style="display: inline-block;width: 16%;margin-top: 10px">
-                    <i class="el-icon-folder" style="font-size: 80px;margin-left: 23%"></i>
+                <div v-for="(item,index) in collection_list" :key="item" @click="viewCollection(item.Did)" style="display: inline-block;width: 16%;margin-top: 10px">
+                    <i class="el-icon-folder" style="font-size: 80px;margin-left: 23%;color: gold"></i>
                     <div style="text-align: center">
                         {{item.name}}
                     </div>
@@ -61,48 +61,25 @@
         data() {
             return {
                 activeIndex: '1',
-                //dialogVisible: false,//删除的dialog
-                //dialogVisible2: false,//新建的dialog
                 select:0,//选择删除哪一个文件夹
-                form: {
-                    id: '123456789',
-                    sid: '',
-                    name: this.$store.state.userid,
-                    email: '',
-                    info: ''
-                },
-                //message数组看实际情况加载内容，比如在“系统通知”则加载系统通知，切换其它目录时清空后重新加载
-                collection_list:[{
-                    id:'',//文件夹id
-                    name:'收藏夹1',//文件夹名字
-                },{
-                    id:'',
-                    name:'收藏夹2',
-                },{
-                    id:'',
-                    name:'收藏夹3',
-                },{
-                    id:'',
-                    name:'收藏夹4',
-                },{
-                    id:'',
-                    name:'收藏夹5',
-                },{
-                    id:'',
-                    name:'收藏夹6',
-                },{
-                    id:'',
-                    name:'收藏夹7',
-                },{
-                    id:'',
-                    name:'收藏夹8',
-                }]
+
+                collection_list:[],
+                paper_list:[]
             }
         },
         created(){
-            this.showuerinfo()
+            this.showCollection()
         },
         methods: {
+            showCollection () {
+                const res= this.$axios({
+                    method:'get',
+                    url:'/showdir'
+                }).catch(err=>{console.log(err)})
+
+                this.collection_list = res.data
+                return 0
+            },
             toIdentify(){
                 this.$router.replace(
                     {path:"",
@@ -117,8 +94,14 @@
                     cancelButtonText: '取消',
                     type: 'warning'
                 }).then(() => {
-                    this.collection_list.splice(index, 1);
+                    //this.collection_list.splice(index, 1);
                     //从collection_list里删除该文件夹
+                    let postData = {
+                        'Did': this.collection_list[index].Did
+                    }
+                    this.$axios.post('/dir/delete', postData).then((response) => {
+                        this.showCollection()
+                    })
                     this.$message({
                         type: 'success',
                         message: '删除成功!'
@@ -130,55 +113,40 @@
                     });
                 });
             },
-            cancel(index) {
-                this.$confirm('是否确定取消对该学者的关注?', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    this.scholar_list.splice(index, 1);
-                    //从scholar_list里删除该学者
-                    this.$message({
-                        type: 'success',
-                        message: '取关成功!'
-                    });
-                }).catch(() => {
-                    this.$message({
-                        type: 'info',
-                        message: '已取消'
-                    });
-                });
-            },
-            viewCollection(index){
-
+            viewCollection(id){
                 this.$router.replace(
-                    {path:'',
+                    {path:'/papercollection',
                         query:{
-                            id:this.$route.query.id
+                            //    id:this.$route.query.id,
+                            Did:id,
                         }
                     })
+
                 //跳转到对应收藏夹页
             },
+            toTeamSpace(teamtitle,teamid){
+
+            },
             async newCollection() {
-                await this.$prompt('请输入文件夹名称', '提示', {
+                await this.$prompt('请输入收藏夹名称', '提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消'
                 }).then(async ({ value }) => {
                     if(!value){
-                        alert('文件夹名不能为空')
+                        alert('收藏夹名不能为空')
                     }
                     else{
                         const res=await this.$axios({
                             type:'params',
                             method:'post',
-                            url:'/team/create',
+                            url:'/makedir',
                             data:{
-                                tname:value
+                                Dname:value
                             }
                         }).catch(err=>{console.log(err)})
                         console.log(res)
 
-                        this.showteamlist()
+                        this.showCollection()
                         this.$message({
                             type: 'success',
                             message: '新建成功'
