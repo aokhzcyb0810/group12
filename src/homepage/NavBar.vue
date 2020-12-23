0105d74f09463508834aaf8ed4fc86f4e31bbc1b
aokhzcyb0810 <63200182+aokhzcyb0810@users.noreply.github.com>
2020-12-23 11:54:29 +0800

    Update NavBar.vue

--- src/homepage/NavBar.vue
+++ src/homepage/NavBar.vue
@@ -194,7 +194,7 @@
                 }
             };
             return{
-                logo: require("../assets/navLogo.png"),
+                logo: require("10.251.253.212:8082/../assets/navLogo.png"),
                 user: !!sessionStorage.getItem('userL'),
                 activeIndex: '1',
                 loginFormVisible: false,
